c4251d98c9e5ce32e4dcb93e23e0d244f6e28b24
Narasimha Kamath <narasimhakamath@outlook.com>
2022-11-07 19:46:20 +0530

    - Added a 404 animation JSON file in the assets. - Added a LottieView for displaying 404 error animation in the invoice details screen where no valid data is presented to the screen component.

--- screens/Invoice.js
+++ screens/Invoice.js
@@ -1,11 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import LottieView from 'lottie-react-native';
 
-const Invoice = () => {
+const Invoice = ({eInvoice}) => {
+	if(!eInvoice) {
+		return(
+			<View style={styles.screen}>
+				<LottieView
+					autoPlay
+					source={require("../assets/404.json")}
+				/>
+			</View>
+		);
+	}
+
 	return(
 		<View>
 			<Text>INVOICE SCREEN</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		backgroundColor: '#000000'
+	}
+});
 
 export default Invoice;
